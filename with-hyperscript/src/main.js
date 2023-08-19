8ccec760b94f86beab5084db811e08b18c99adb5
hamza761 <hamzazahid761@gmail.com>
2023-08-19 19:56:56 +0500

    templates

---
+++ with-hyperscript/src/main.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "testing-extension-ui";
+import App from './App.vue'
+import Testing from './Testing.vue'
+initializeApp({
+    drawer: App,
+    testing: Testing
+})
---
+++ with-jsx/src/Testing.vue
@@ -0,0 +1,11 @@
+<script lang="jsx">
+import { Typography } from 'testing-extension-ui';
+
+export default {
+  setup() {
+    return () => {
+      return <><Typography>Testing Components is here</Typography></>
+    }
+  }
+}
+</script>
