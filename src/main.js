f1e15dde079cedcae828b8af6dff57c288392f74
hamza761 <hamzazahid761@gmail.com>
2023-09-19 23:24:56 +0500

    options added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "testing-extension-ui";
 import App from './App.vue'
 import Testing from './Testing.vue'
+import options from './options.json'
 initializeApp({
     drawer: App,
     testing: Testing
-})
+}, options)
